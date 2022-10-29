aa64fc359429bc31aac3b4996c3a229ac9379dba
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-10-29 16:49:36 -0400

    added comments

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const solver = () => {//Set block
+const solver = (sudoku) => {//Set block
 
     const getBlockNumber = (row, col) => {
         let block = "";
-        row < 3
-            ? col < 3
-                ? block = 1
-                : col < 6
-                    ? block = 2
-                    : block = 3
+        row < 3     //if in first 3 rows
+            ? col < 3   //and first three rows
+                ? block = 1     //you are in block 1
+                : col < 6       //if first 3 rows and next three columns
+                    ? block = 2        //you are in block 2
+                    : block = 3     //if in first 3 rows and last 3 columns, you are block 3
             : row < 6
                 ? col < 3
                     ? block = 4
@@ -23,21 +23,36 @@ const solver = () => {//Set block
     }
 
     const createHash = (sudoku) => {
+        //initialize map object
         const map = {};
+        //loop through each row
         for (let row = 0; row < 9; row++) {
+            //loop through each column in each row
             for (let col = 0; col < 9; col++) {
+                //retrieve the value from the sudoku array
                 const value = sudoku[row][col];
+                //ignore empty spaces
                 if (value === ".") continue;
+                //check if value has previously been added to hash map
                 if (map[value]) {
+                    /* the key will be the number itself
+                    under rows, the row number will be added as the key
+                    with a value of true */
                     map[value]["rows"][row] = true;
+                    //col added as key with value true
                     map[value]["cols"][col] = true;
+                    //getBlockNumber is called so it can be added as a key
                     map[value]["blocks"][getBlockNumber(row, col)] = true;
                 } else {
+                    /*if the number has not been previously added then the 
+                    number is added as a key and the row, cols, blocks objects
+                    are initialized*/
                     map[value] = {
                         rows: {},
                         cols: {},
                         blocks: {}
                     }
+                    //the row, col and block number keys are added with value of true
                     map[value]["rows"][row] = true;
                     map[value]["cols"][col] = true;
                     map[value]["blocks"][getBlockNumber(row, col)] = true;
@@ -56,49 +71,69 @@ const solver = () => {//Set block
 
     const checkSurroundings = (col, row, num, map) => {
         let rowEnd, colEnd;
-        let openings = 0;
 
+        /*
+            We start by setting the limits for our loop. We are only interested in
+            going through the fields in our current block.
+        */
         row < 3 ? rowEnd = 3 : row < 6 ? rowEnd = 6 : rowEnd = 9; //set max for row loop
         col < 3 ? colEnd = 3 : col < 6 ? colEnd = 6 : colEnd = 9; //set max for col loop
 
-        for (let i = rowEnd - 3; i < rowEnd; i++) {
-            for (let j = colEnd - 3; j < colEnd; j++) {
+        for (let i = rowEnd - 3; i < rowEnd; i++) { //loop through each row
+            for (let j = colEnd - 3; j < colEnd; j++) { //loop through each column
                 const currentFieldValue = sudoku[i][j];
 
+                //ignore the field we are currently in. We've already checked that field
                 if (i === row && j === col) continue;
+                
+                //If there's already a numbere at this position, no need to check
                 if (currentFieldValue !== ".") continue;
 
                 const canBePlaced = canNumberBePlaced(j, i, num, map);
-                if (canBePlaced) openings++
+
+                /*
+                if we can place our number in any other field we can return false so the
+                solver can try the next number or posision.
+                */
+                if (canBePlaced) return false
 
             }
         }
-
-        return openings === 0;
+        /*If we made it through every field and the number has no other posibilities,
+        we have found it a home. */
+        return true;
     }
+
+    //Call function to create our hash map. Ready to use it.
     const map = createHash(sudoku);
 
     for (let row = 0; row < 9; row++) { //loop through row
         for (let col = 0; col < 9; col++) { //loop through column
-            const currentFieldValue = sudoku[row][col];  //current field in sudoku
+
+            const currentFieldValue = sudoku[row][col];  //retrieve value from current field in sudoku
+
             if (currentFieldValue !== ".") continue; //if a number exist in field move to next column
-            for (let i = 1; i < 10; i++) {  //checking numbers 1-9 one at a time
-                if (!map[i]) continue;   //if not in map at all, move to next number
+
+            for (let i = 1; i < 10; i++) {  //checking numbers 1-9 one at a time in current position
+                if (!map[i]) continue;   //if not in map at all, move to next number.
+
                 const canBePlaced = canNumberBePlaced(col, row, i, map);  //true means number can be placed here
-                if (!canBePlaced) continue;
+
+                if (!canBePlaced) continue;     //if number can't be placed here, try next number
+
                 const numberBelongs = checkSurroundings(col, row, i, map);   //true means number can ONLY be placed here.
 
-                if (canBePlaced && numberBelongs) {
-                    sudoku[row].splice(col, 1, i);
-                    return solver(sudoku);
+                if (canBePlaced && numberBelongs) { //if number can be placed here and nowhere else
+                    sudoku[row].splice(col, 1, i);  //add number to our sudoku
+                    return solver(sudoku);  //call solver function again and pass our updated sudoku array.
                 }
             }
         }
     }
-    return sudoku;
+    return sudoku; //if no new number can be added we should have  solved puzzle.
 }
 
-const sudoku = [
+let sudoku = [
     ["5", "3", ".", ".", "7", ".", ".", ".", "."],
     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
     [".", "9", "8", ".", ".", ".", ".", "6", "."],
@@ -110,29 +145,23 @@ const sudoku = [
     [".", ".", ".", ".", "8", ".", ".", "7", "9"]
 ]
 
-const solvedSudoku = solver(sudoku);
+let solvedSudoku = solver(sudoku);
 console.table(solvedSudoku);
 
-let rowCheck = false
-solvedSudoku.forEach((row) => {
-    for (let i = 1; i < 10; i++) {
-        rowCheck = (row.includes(i) || row.includes(String(i)));
-    }
+sudoku = [
+    ["7","9","4",".","8","6","3","1","."],
+    ["1",".","2",".","7","3","5","8","9"],
+    [".","5",".",".","9","2",".",".","."],
+    [".",".",".","7","5",".","1","2","."],
+    [".",".","7","3",".",".",".","9","6"],
+    [".","4",".",".","1",".",".",".","."],
+    ["4","3",".",".",".",".",".","5","1"],
+    [".",".",".","9",".",".","6",".","."],
+    ["6",".","1",".","3",".",".",".","."]
+]
 
-})
-console.log({ rowCheck });
-
-let colCheck = false;
-
-for (let col = 0; col < 9; col++) {
-    solvedSudoku.forEach((row) => {
-        for (let i = 1; i < 10; i++) {
-            colCheck = Number(row[col]) === i;
-        }
-    })
-}
-
-console.log({ colCheck });
+solvedSudoku = solver(sudoku);
+console.table(solvedSudoku);
 
     //solution
     // ["5", "3", 4, 6, "7", 8, 9, 1, 2],
